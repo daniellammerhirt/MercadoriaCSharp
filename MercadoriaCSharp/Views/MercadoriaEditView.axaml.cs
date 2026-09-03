fac142110e3281525e4b3ac7524104a72369ae96
daniellammerhirt <daniellammer75@gmail.com>
2026-09-03 15:30:05 -0300

    init: inicia projeto

---
+++ MercadoriaCSharp/Views/MercadoriaEditView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MercadoriaCSharp.Views;
+
+public partial class MercadoriaEditView : Window
+{
+    public MercadoriaEditView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MercadoriaCSharp/Views/SaidaEditView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MercadoriaCSharp.Views;
+
+public partial class SaidaEditView : Window
+{
+    public SaidaEditView()
+    {
+        InitializeComponent();
+    }
+}
